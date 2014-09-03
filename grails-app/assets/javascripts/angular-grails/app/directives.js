a38936962be7eb68f788b8101e1b340359ddc4a6
DieMyst <somedayofmylife@gmail.com>
2014-09-04 00:51:15 +0400

    some experiments with html. need to draggable and resizable div over iframe, that change position of iframe

--- grails-app/assets/javascripts/angular-grails/app/directives.js
+++ grails-app/assets/javascripts/angular-grails/app/directives.js
@@ -8,7 +8,21 @@ playerDirectives.directive('resizableDraggable', function () {
         resize: function (event, ui) {
             var newWd = ui.size.width - 30;
             var newHt = ui.size.height - 30;
-            ui.element.find("iframe").width(newWd).height(newHt);
+            $(this).parent().width(newWd).height(newHt);
+            $(this).parent().find("iframe").width(newWd).height(newHt);
+        }
+    };
+
+    var draggableConfig = {
+        drag: function (event, ui) {
+            var posLeft = ui.position.left;
+            var posTop = ui.position.top;
+
+            var parentEl = $(this).parent();
+            var parentPosTop = parentEl.position().top;
+            var parentPosLeft = parentEl.position().left;
+            parentEl.css({top:  parentPosTop + posTop, left: parentPosLeft + posLeft});
+            $(this).css({top: 0, left: 0});
         }
     };
 
@@ -18,7 +32,10 @@ playerDirectives.directive('resizableDraggable', function () {
             callback: '&onResize'
         },
         link: function postLink(scope, elem) {
-            elem.resizable(resizableConfig).draggable();
+            elem.resizable(resizableConfig).draggable(draggableConfig);
+            if (elem.is('.ui-draggable') || (/absolute/).test(elem.css('position'))) {
+                elem.css({position: 'absolute'});
+            }
         }
     };
 });
